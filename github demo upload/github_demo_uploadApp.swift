a333ff5a548e1c51716493d408cce2fd84746163
mohammed adel <hamadygg66@gmail.com>
2023-06-24 14:56:20 +0400

    Initial Commit

---
+++ github demo upload/github_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demo_uploadApp.swift
+//  github demo upload
+//
+//  Created by mohmmed adel on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct github_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
